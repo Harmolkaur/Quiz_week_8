259c9d999ced41d5ae1a4ca1c2a724fd91fe51a2
Harmol Kaur <harmolkaur@griffithuni.edu.au>
2023-09-16 21:31:48 +1000

    My first commit from local machine

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,29 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
+
+#Connect to SQLite database
+connect = sqlite3.connect('climate.db')
+
+#create a cursor object to interact with the database 
+cursor = connect.cursor()
+
+#A query to fetch data from the database 
+cursor.execute('SELECT year, co2, temperature FROM ClimateData ORDER BY year')
+
+#Fetch rows from query
+data = cursor.fetchall()
+
+# Populate the lists with the retrieved data
+for row in data:
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+connect.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
